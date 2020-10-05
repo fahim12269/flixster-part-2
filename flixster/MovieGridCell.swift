da7575d0e7901fc4e5ddcfd2fdf4070788ed9216
fahim12269 <55300345+fahim12269@users.noreply.github.com>
2020-10-05 17:18:55 -0400

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Farhadul Fahim on 10/5/20.
+//  Copyright © 2020 Farhadul Fahim. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
